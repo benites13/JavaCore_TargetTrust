02a3723584d25f62820a3f9ee52988cd8f1112e3
diego <diegovb85@gmail.com>
2024-08-16 14:20:49 -0300

    Mais um teste

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Diego Benites");
         System.out.println("Teste de Hoje");
+        System.out.println("Mais um teste");
     }
 }
