c812d51989ab7c5955861bec52212570c7ac8584
diego <diegovb85@gmail.com>
2024-08-16 19:03:59 -0300

    Mais um teste

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,23 @@
 public class Main {
+    public static int max_value = 100;
+    public static String FUNCTION_NAME = "Main function";
+
+
+    // Exemplo de chamada de methodo não estatica
     public static void main(String[] args) {
-        System.out.println("Diego Benites");
-        System.out.println("Teste de Hoje");
-        System.out.println("Mais um teste");
+        System.out.println("Hello world!");
+        // instanciando objetos do tipo MinhaCalculadora
+        var objetoCalculadora = new MinhaCalculadora();
+        MinhaCalculadora objetoCalculadora2 = new MinhaCalculadora();
+
+        // Acessando methodos dentro dos objetos
+        var somatorio = objetoCalculadora.soma(2, 4);
+        int subtracao = objetoCalculadora2.subtrair(4, 5);
+
+        System.out.println("O somatorio é: " + somatorio);
+        System.out.println("A subtração é " + subtracao);
+
     }
+
+
 }
