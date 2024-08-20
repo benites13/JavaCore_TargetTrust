0800d3f94ecc977d41147e1e05fff1af556d0603
diego <diegovb85@gmail.com>
2024-08-19 21:39:01 -0300

    Aula 3 - Calculadora IMC Com classificação

--- src/CalculaIMC.java
+++ src/CalculaIMC.java
@@ -2,17 +2,31 @@ import java.util.Scanner;
 
 public class CalculaIMC {
     public static void main(String[] args) {
-        Scanner inputAltura = new Scanner(System.in);
-        Scanner inputPeso = new Scanner(System.in);
+        Scanner inputTeclado = new Scanner(System.in);
         System.out.print("Informar altura: ");
-        float altura = inputAltura.nextFloat();
+        float altura = inputTeclado.nextFloat();
         System.out.print("Informar peso: ");
-        float peso = inputPeso.nextFloat();
+        float peso = inputTeclado.nextFloat();
 
         float valorIMC = peso / (altura * altura);
 
         String resultadoIMC = String.format("%.2f", valorIMC);
+        String classificacao = "";
 
-        System.out.print(resultadoIMC);
+        if(valorIMC < 18.5){
+            classificacao = "Magreza";
+        }else if(valorIMC >= 18.5 && valorIMC < 24.9){
+            classificacao = "Normal";
+        }else if(valorIMC >= 25 && valorIMC < 29.9){
+            classificacao = "Sobrepeso";
+        }else if(valorIMC >= 30 && valorIMC < 39.9){
+            classificacao = "Obesidade";
+        }else if(valorIMC >= 40){
+            classificacao = "Obesidade Grave";
+        }
+
+        System.out.print(resultadoIMC + ": " + classificacao);
+
+        inputTeclado.close();
     }
 }
